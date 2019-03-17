69b9782a0dc8152f9311ef48e94e85cbfb8b5518
collandhard <836053877@qq.com>
2019-03-17 21:10:40 +0800

    www

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<?php
+echo "kangshao";
+$arr4=array(1=>3,'a1'=>11,"mn"=>18,88=>2);
+$v1=current($arr4);
+$v2=key($arr4);
+echo $v1;
+echo "<br/>";
+echo "$v2";
+?>
+</html>
